eb3a899fdc8e9de73426c7de4dbd3758cfd225c1
Julian Cuentas <juliancuentas@hotmail.com>
2020-05-03 15:54:30 -0300

    first commit: Preparing the app, and accesing context with contextType

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import Navbar from './components/navbar.component';
+import BookList from './components/booklist.component';
+import ThemeContextProvider from './contexts/ThemeContext';
+
+//Navbar and BookList are attached to the props of the ThemeContextProvider (father) 
+//so we have to pass the props down to the children {this.props.children}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeContextProvider>
+      <Navbar/>
+      <BookList/>
+      </ThemeContextProvider>
     </div>
   );
 }
