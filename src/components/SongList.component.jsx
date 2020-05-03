a767407e25fa6a787e4a3f31e5bd0730bd5fe646
Julian Cuentas <juliancuentas@hotmail.com>
2020-05-03 20:03:41 -0300

    added useState and mapped through the state in order to render the <li>

--- src/components/SongList.component.jsx
+++ src/components/SongList.component.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+// package for ids
+import  uuid  from "uuid/dist/v1";
 
 const SongList = () => {
+    const [songs, setSongs] = useState([
+        {title: 'Las habladurias del mundo', id:uuid()},
+        {title: 'Perdonado', id:uuid()},
+        {title: 'Cancion para los dias de la vida', id:uuid()},
+    ]);      
+
+    // setSongs will change and completeley replace
+    const addSong = () => {
+        setSongs([...songs, {title: "new song", id: uuid()}])
+    }
     return ( 
     <div className="song-list">
         <ul>
-            <li>Memory gospel</li>
-            <li>Perdonado</li>
+            {songs.map(song => 
+            <li key={song.id}>{song.title}</li>)}
         </ul>
+        <button onClick={addSong}>Add song</button>
     </div> );
 }
  
