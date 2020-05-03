1d15d071062987f52cae4ffec7f9c49c5bb3b42c
Julian Cuentas <juliancuentas@hotmail.com>
2020-05-03 18:06:25 -0300

    video6 added ThemeToggle component and used setState to change the state and re-render with new theme

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Navbar from './components/navbar.component';
 import BookList from './components/booklist.component';
 import ThemeContextProvider from './contexts/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
 
 //Navbar and BookList are attached to the props of the ThemeContextProvider (father) 
 //so we have to pass the props down to the children {this.props.children}
@@ -12,6 +13,7 @@ function App() {
       <ThemeContextProvider>
       <Navbar/>
       <BookList/>
+      <ThemeToggle/>
       </ThemeContextProvider>
     </div>
   );
