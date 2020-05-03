8436d9657837f5a2d02d88c49b8f1d7a8ef00366
Julian Cuentas <juliancuentas@hotmail.com>
2020-05-03 20:47:06 -0300

    added useEffect and showed how to use it to track for different state changes

--- src/components/SongList.component.jsx
+++ src/components/SongList.component.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect } from 'react';
 
 // package for ids
 import  uuid  from "uuid/dist/v1";
@@ -9,12 +9,21 @@ const SongList = () => {
         {title: 'Las habladurias del mundo', id:uuid()},
         {title: 'Perdonado', id:uuid()},
         {title: 'Cancion para los dias de la vida', id:uuid()},
-    ]);      
+    ]);     
+    
+    const [age, setAge] = useState(20);
 
-    // setSongs will change and completeley replace
     const addSong = (title) => {
         setSongs([...songs, {title: title, id: uuid()}])
     }
+    //running everytime or everytime the data in the component changes inside
+    // I pass an araay, as second parameters, to things to check
+    useEffect(() => {
+        console.log('useEffect hook ran', songs);
+    }, [songs])
+    useEffect(() => {
+        console.log('useEffect hook ran', age);
+    }, [age])
     return ( 
     <div className="song-list">
         <ul>
@@ -22,6 +31,7 @@ const SongList = () => {
             <li key={song.id}>{song.title}</li>)}
         </ul>
         <NewSongFrom addSong={addSong}/>
+            <button onClick={() => setAge(age + 1)}>Add 1 to age: {age}</button>
     </div> );
 }
  
