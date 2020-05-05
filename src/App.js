a8b1a6c7261a82cd1dfd8e1b34491030aba91375
Julian Cuentas <juliancuentas@hotmail.com>
2020-05-05 11:53:33 -0300

    v17

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import BookContextProvider from './contexts/BookContext';
 import NavBar from './components/NavBar.component';
+import BookList from './components/BookList.component';
 
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
         <NavBar/>
+        <BookList/>
       </BookContextProvider>
     </div>
   );
