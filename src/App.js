627165675c879ac0d2e998977517fe1996340bb9
Julian Cuentas <juliancuentas@hotmail.com>
2020-05-11 16:57:19 -0300

    u can erase this

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import SongList from "./components/SongList.component";
 
+
 function App() {
   return (
     <div className="App">
-      <SongList />
+      
+      <SongList/>
     </div>
   );
 }
